63ed35500d5d33e82a60cd07a25befb75bb646cb
Dandelion16012 <shchekinapa@yandex.ru>
2023-10-04 12:10:12 +0300

    почему-то нет доступа к сайту, все запросы в коллекцию выполнены

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -15,13 +15,14 @@ namespace lab6
             Random random = new Random();
 
             int n = 0;
-           // while (n< 50)
+            while (n< 50)
             {
                 double latitude= -90+random.NextDouble()*(90+90);
                 double longitude = -180 + random.NextDouble() * (180 + 180);
-                string Key = "9efa86170885592ef3b1450c671eb1bc";
+                string Key = "d8499591d06290612897d7f0c9290496";
                 //exclude- исключаем ненужные параметры, metric- используем стандартные единицы измерения
-                string url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude.ToString() + "&" +"lon=" + longitude.ToString() + "&exclude=minutely,hourly,daily,alerts&" +"units=metric&appid=" + Key;
+                string url = "https://api.openweathermap.org/data/3.0/onecall?lat=" + latitude.ToString() + 
+                    "&" +"lon=" + longitude.ToString() + "&exclude=minutely,hourly,daily,alerts&" +"units=metric&appid=" + Key;
                              
                 HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);// создаём объект реквеста для возможности запроса
                 HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();// получаем респонс, считываем данные с реквеста
@@ -31,10 +32,10 @@ namespace lab6
                 // using чтобы убрать все используемые им ресурсы, наследуется от класса, который реализует интерфейс
                 {
                     response = streamReader.ReadToEnd();
-                    //обрабатываем json в сущность с#, дисереализуем строку и превращаем в объект
+                    //обрабатываем json в сущность с#, десереализуем строку и превращаем в объект
                 }
                 WeatherResponce weatherResponse=JsonConvert.DeserializeObject<WeatherResponce>(response);
-                //Console.WriteLine($"Temperature in {weatherResponse.Name}: {weatherResponse.main.Temp}");
+                Console.WriteLine($"Temperature in {weatherResponse.Name}: {weatherResponse.main.Temp}");
 
                 if  (weatherResponse.Name != "" || weatherResponse.sys.Country != "")
                 {
@@ -43,13 +44,38 @@ namespace lab6
                     Console.WriteLine($"Temperature in {weather.Name}: {weather.Temp}");
                     n++;
 
-                }                    
-                
+                }
+                var linqMaxTemp = weathers.MaxBy(weather=>weather.Temp).Country;
+                Console.WriteLine($"страна с самиой высокой температурой: {linqMaxTemp}");
+                var linqMinTemp = weathers.MinBy(weather => weather.Temp).Country;
+                Console.WriteLine($"страна с самиой низкой температурой: {linqMinTemp}");
+
+                /*  var linqDescription1 = from weather in weathers
+                                         where weather.Description == "clear sky" ||
+                                         weather.Description == "rain" ||
+                                         weather.Description == "few clouds"
+                                         select weather;*/
+                var linqCountry= weathers.Select(weather=>weather.Country).Distinct().ToList();
+                Console.WriteLine($"уникальных стран: {linqCountry.Count}");
+
+                var linqAverTemp = weathers.Average(weather => weather.Temp);
+                Console.WriteLine($"средняя температура в мире: {linqAverTemp}");
+
+                var linqDescription = weathers.FirstOrDefault(weather => weather.Description == "clear sky" ||
+                                      weather.Description == "rain" ||
+                                      weather.Description == "few clouds");
+                Console.WriteLine($"Первая страна с определёнными значениями Description: {linqDescription.Country}, " +
+                    $"{linqDescription.Name}, " +
+                    $"discr: {linqDescription.Description}");
+
+
+
+
             }
             Console.WriteLine();
 
         }
 
     }
-   
+
 }
